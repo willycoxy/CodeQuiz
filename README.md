127d614abeb6f936d76d0c2911ad5294f612f96c
willycoxy <williamjwcox@gmail.com>
2022-09-25 14:12:42 -0400

    files created

---
+++ README.md
@@ -0,0 +1,9 @@
+# Javascript Code Quiz
+
+## Built With 
+* Javascript
+* HTML
+* CSS
+## Purpose 
+This is a website that prompts you gernal knowledge questions about javascript. At the end of the questions you will be given a score based on how well you did.
+## Website 
---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+ // questions that will appear in quiz
+
+var questions = [
+    {
+        question: "Inside which HTML element do we put the javascript?",
+        choices: ["<script>", "<scripting>", "<js>","<javascript>"],
+        answer: "<script>"
+    },
+    {
+        question: "How does a WHILE loop start?",
+        choices: ["while i=1 to 10", "while(i <= 10)", "while (i<= 10; i++)"],
+        answer: "while(i <= 10)"
+    },
+    {
+        question: "How do you add an comment in javascript?",
+        choices: ["//This is a comment", "'This is a comment", "<!--This is a comment-->"],
+        answer: "//This is a comment"
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["rnd(7.25)", "round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)"],
+        answer: "Math.round(7.25)"
+    },
+    {
+        question: "JavaScript is the same as Java.",
+        choices: ["True", "False"],
+        answer: "False"
+    }
+]
